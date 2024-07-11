adcc306bad38d3ace2b4fc3684e3ae86930eed46
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-07-11 17:26:02 +0530

    main

--- src/data.js
+++ src/data.js
@@ -6,6 +6,67 @@ export const data = [
        desc: 'Let Cats Jump on Screen',
        link : 'https://cat-bounce.com/'
     },
+    {
+        id: 2,
+         icon: 'http://www.google.com/s2/favicons?domain=pointerpointer.com',
+            name: 'Pointer Pointer',
+            desc: 'Find the Pointer',
+            link : 'https://pointerpointer.com/'
+    },
+    {
+        id : 3,
+        icon: 'http://www.google.com/s2/favicons?domain=theuselessweb.com',
+        name: 'The Useless Web',
+        desc: 'Take me to a useless website',
+        link : 'https://theuselessweb.com/'
+    },
+    {
+        id : 4,
+        icon: 'http://www.google.com/s2/favicons?domain=heeeeeeeey.com',
+        name: 'Heeeeeeeey',
+        desc: 'Heeeeeeeey',
+        link : 'https://heeeeeeeey.com/'
+    },
+    {
+        id:5,
+        icon: 'http://www.google.com/s2/favicons?domain=beesbeesbees.com',
+        name: 'Bees Bees Bees',
+        desc: 'Bees Bees Bees',
+        link : 'https://beesbeesbees.com/'
+    },
+    {
+        id:6,
+        icon: 'http://www.google.com/s2/favicons?domain=corndog.io',
+        name: 'Corn Dog',
+        desc: 'Corn Dog',
+        link : 'https://corndog.io/'
+    },
+    {
+        id:7,
+        icon: 'http://www.google.com/s2/favicons?domain=omfgdogs.com',
+        name: 'OMFG Dogs',
+        desc: 'OMFG Dogs',
+        link : 'https://omfgdogs.com/'
+    },
+    {
+        id:8,
+        icon : 'http://www.google.com/s2/favicons?domain=endless.horse',
+        name : 'Endless horse',
+        desc : 'Endless horse',
+        link : 'https://endless.horse/'
+    },
+    {
+        id:9,
+        icon : 'http://www.google.com/s2/favicons?domain=zoomquilt.org',
+        name : 'Zoom Quilt',
+
+        desc : 'Zoom Quilt',
+        link : 'https://zoomquilt.org/'
+    },
+    {
+        id:10,
+        
+    }
  
     
 
